f7e3780b712d8890582b9c2f368a84023ad314c2
Yaremko Ivan <yaremich98@gmail.com>
2021-10-04 09:42:55 +0300

    Add object building

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
-const cat = {
-    name: 'Mukha',
-    isMale: false,
-    color: 'black and white',
-    breed: 'yard',
-    age: 2,
-    isSlipping: true,
-    talk:function(){
-        return 'meow';
-    },
-};
+// const cat = {
+//     name: 'Mukha',
+//     isMale: false,
+//     color: 'black and white',
+//     breed: 'yard',
+//     age: 2,
+//     isSlipping: true,
+//     talk:function(){
+//         return 'meow';
+//     },
+// };
 
-cat.age++;
-console.log(cat.talk());
-cat.countPaws = 4;
-console.log(cat);
-console.log(cat.name);
-console.log(cat.isMale);
-console.log(cat.age);
+// cat.age++;
+// console.log(cat.talk());
+// cat.countPaws = 4;
+// console.log(cat);
+// console.log(cat.name);
+// console.log(cat.isMale);
+// console.log(cat.age);
 
-delete cat.isSlipping;
-console.log(cat);
+// delete cat.isSlipping;
+// console.log(cat);
+
+
+// const building = {
+//     name: 'House',
+//     isBrick: true,
+//     color: 'orange and green',
+//     district: 'Alexandrovskiy',
+//     flours: 12,
+//     elevator:function(){
+//         return 'up';
+//     },
+// };
+
+// console.log(building.elevator());
+// console.log(building);
+// console.log(building.name);
+// console.log(building.isBrick);
+// console.log(building.flours);
