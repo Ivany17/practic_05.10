05f906e70d257c7bba499894ffddac4084000cfa
Yaremko Ivan <yaremich98@gmail.com>
2021-10-05 11:59:32 +0300

    Add selfwork about table

--- index.js
+++ index.js
@@ -166,13 +166,36 @@
 // console.log(sayHello(user));
 
 
-const obj = {
-    name: 'pen',
-    'is written': true,
-    11: 78,
-}
+// const obj = {
+//     name: 'pen',
+//     'is written': true,
+//     11: 78,
+// }
 
-console.log(obj[11]);
-console.log(obj['11']);
-console.log(obj['is written']);
-console.log(obj['name']);
+// console.log(obj[11]);
+// console.log(obj['11']);
+// console.log(obj['is written']);
+// console.log(obj['name']);
+
+
+        // const createMultiplicationTable = function(min=1, max=10){
+        //     const table = {};
+        //     for(let i=min; i<=max; i++){
+        //         for(let j=1; j<=max; j++){
+        //             //Доступ к свойству с возможностью вычисления
+        //             table[`${i} * ${j} = `] =  i*j;
+        //         }
+        //     }
+        //     return table;
+        // }
+
+        // console.log(createMultiplicationTable())
+
+
+            // const f = function(str, obj){
+            //     return obj[str];
+            // }
+
+            // const str = '2 * 3 = ';
+            // const table = createMultiplicationTable();
+            // console.log(f(str, table));
