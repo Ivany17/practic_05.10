f58f3331596516405df6327dca900568c6468943
Yaremko Ivan <yaremich98@gmail.com>
2021-10-06 11:49:31 +0300

    Refuctor constructor

--- index.js
+++ index.js
@@ -199,3 +199,34 @@
             // const str = '2 * 3 = ';
             // const table = createMultiplicationTable();
             // console.log(f(str, table));
+
+
+function MyArrayProto(){
+    this.push = function(){
+        for(let i = 0; i < arguments.length; i++){
+            this[this.length++] = arguments[i];
+        }
+        return this.length;
+    }
+    this.pop = function(){
+        if(this.length===0){
+            return;
+        }
+        const item = this[--this.length];
+        delete this[this.length];
+        return item;
+    }
+}
+
+function MyArray(){
+    this.length = 0;
+    for(let i = 0; i < arguments.length; i++){
+        this.push(arguments[i]);
+    }
+}
+
+MyArray.prototype = new MyArrayProto();
+MyArray.prototype.newMethod = function(){};
+
+const myArray = new MyArray(1,1,1,15,5);
+myArray.push(2,2,2);
